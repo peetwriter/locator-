01477f70621ad6cb6ec346d6593aa04abfba35ac
Petr Tarasenko <pereter92@gmail.com>
2017-03-21 00:11:21 +0200

    webpack added with express dev inv

--- src/index.js
+++ src/index.js
@@ -1,9 +1,27 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+
+import Map from './components/MapContainer.js'
+import logo from './logo.svg';
+import './App.css';
+
 import './index.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <div className="App-header">
+          <h2>Welcome to locator</h2>
+        </div>
+        <Map/>
+      </div>
+    );
+  }
+}
 
 ReactDOM.render(
   <App />,
   document.getElementById('root')
 );
+// export default App;
---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+module.exports = {
+  entry: __dirname + '/src',
+  output: {
+    path: '/'
+  },
+  devtool: 'source-maps',
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        loader: 'babel',
+        query: {
+          presets: ['es2015', 'react'],
+          plugins: ['transform-class-properties']
+        }
+      },
+      {
+        test: /\.css$/,
+        exclude: /node_modules/,
+        loader: 'style-loader!css-loader',
+      },
+      { test: /\.svg$/, loader: 'svg-loader' }
+    ]
+  }
+}
